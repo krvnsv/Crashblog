e77cbd34baf23816ec0aaa0d56e6d813cb0de56d
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-09-06 21:55:19 +0200

    comment functionality

--- crashblog/blog/models.py
+++ crashblog/blog/models.py
@@ -14,4 +14,6 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+#48:05
--- crashblog/blog/views.py
+++ crashblog/blog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import CommentForm
 from .models import Post
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug) # 1st slug ref to Post model, 2nd to func attribute
 
-    return render(request, 'blog/detail.html', {'post': post})
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
