7dd5f49d749268f0d32a699d9dcc3cd99e1608c8
KikiRiki004 <krivonosovk.dev@gmail.com>
2025-09-07 04:58:54 +0200

    implement categories

--- crashblog/blog/models.py
+++ crashblog/blog/models.py
@@ -1,6 +1,20 @@
+from tabnanny import verbose
+from unicodedata import category
 from django.db import models
 
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title', )
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -10,10 +24,12 @@ class Post(models.Model):
     class Meta:
         ordering = ('-created_at',) # decending order of created_at
 
+    def __str__(self):
+        return self.title
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-#48:05
